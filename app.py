f29a09000713cd8d5a0b5bae44895ecfff1d7394
Somesh Ghosh <someshghoshjoyguru@gmail.com>
2024-05-19 05:34:14 +0530

    cors

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 load_dotenv()  # Load environment variables
 
